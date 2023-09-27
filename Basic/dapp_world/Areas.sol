f5169d26771c44e655288d8070bb61c6aa731669
vignesh <vigneshwar.sigamani@gmail.com>
2023-09-27 10:04:53 +0530

    dapp world area

---
+++ Basic/dapp_world/Areas.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract CalculateArea {
+    //this function returns area of square
+    function squareArea(uint a) public pure returns (uint) {
+        require(a > 0, "side is zero");
+        return a * a;
+    }
+
+    //this function returns area of rectangle
+    function rectangleArea(uint a, uint b) public pure returns (uint) {
+        require(a > 0 && b > 0, "one of the side is zero");
+        return a * b;
+    }
+}
